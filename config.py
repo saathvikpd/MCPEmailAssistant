ee9a86b83f87b8d882f1364d8692f623c34d3d79
saathvikpd <saathvik.pd@gmail.com>
2025-05-25 18:00:04 -0700

    update gitignore

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
-PROJECT_DIR = "ADD ABSOLUTE PATH TO PROJECT HERE"
-CREDENTIALS_FP = os.path.join(PROJECT_DIR, "ADD NAME OF CLIENT CREDENTIALS FILE")
+PROJECT_DIR = "/Users/saathvikdirisala/Documents/MCPEmailAssistant/emailassistant/"
+CREDENTIALS_FP = os.path.join(PROJECT_DIR, "client_secret_396371009840-fqddcngmeu6m5cp0j6l1qe3t9s4jgkih.apps.googleusercontent.com.json")
 TOKEN_FP = os.path.join(PROJECT_DIR, "token.json")
